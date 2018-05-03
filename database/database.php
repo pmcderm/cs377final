9766c590d318bf6ace268ad118abaa299ab4d965
root <root@csfinal.c.natural-terra-202820.internal>
2018-05-03 16:13:51 +0000

    Second update

--- database/database.php
+++ database/database.php
@@ -1,7 +1,7 @@
 <?php
 class DBConnection
 {
-STATIC $connection = NULL;
+private static $connection =  NULL;
 
 private function connect() {
 $servername = "localhost";
@@ -16,15 +16,52 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+//echo "Connected successfully";
 return $conn;
 }
 
-function getInstance(){
-if($connection == NULL) {
-$connection = $this->connect();
+function getInstance() {
+	if(self::$connection == NULL) {
+		 self::$connection = $this->connect();
+	}
+	return self::$connection;
 }
-return $connection;
+
+//TODO select statement that verifies user exists
+function isUserValid($userName) {
+	$sql = "SELECT * from Student where Sid = ?";
+	$stmt = self::$connection->prepare($sql);
+	$bool = $stmt->bind_param("s", $userName);
+	if(!$bool) {
+		return false;
+	}
+	$result = $stmt->execute();
+	$stmt->close();
+	if($result['num_rows'] > 0){
+		echo "It is true";
+		return true;
+	} else {
+		echo "It is false";
+		return false;
+	}
 }
+
+//TODO select statement that verifies user can provide class evaluation
+// User can only provide one class evaluation
+function isUserEvaluationValid($userName, $evalId) {
+
+}
+
+//TODO select statement that loads the evaluation questions for each course
+function getEvaluationQuestion($course) {
+
+}
+
+//TODO insert evaluation answer
+function insertEvaluationAnswer() {
+
+}
+
+
 }
 ?> 
--- html/info.php
+++ html/info.php
@@ -2,9 +2,27 @@
 <body>
 <?php
 include '../database/database.php';
-$foo = new DBConnection;
-$conn = $foo->getInstance();
-$conn->close();
+	$foo = new DBConnection;
+	$conn = $foo->getInstance();
+	$isUserValid = false;
+	if (!empty($_GET['userName'])) {
+		$userName =  $_GET['userName'];
+		$isUserValid = $foo->isUserValid($userName);
+		echo $isUserValid;
+	}
 ?>
+<?php if (empty($_GET['userName']) || !$isUserValid) : ?>
+	<form action="info.php" method="get">
+		<label for="userName">Enter Username</label>
+		<input type="text" name="userName"/>
+		<button>Submit</button>
+	</form>
+	<?php if ($isUserValid) {
+		echo "User is not valid";
+	} ?>
+<?php else : 
+	echo $userName . " is a valid user name";
+?>
+<?php endif; ?>
 </body>
 </html>
