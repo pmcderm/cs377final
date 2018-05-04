8ca39a8bd45a635846b5a6586c7ce1be0d0cf5b0
Paul McDermott <paulmcdermott@paul-macbook-pro.wireless.emory.edu>
2018-05-03 22:56:41 -0400

    added a bunch of php functions

--- database/database.php
+++ database/database.php
@@ -70,7 +70,7 @@ function isUserValid($userName) {
 
 //TODO get users classes
 function getUsersClasses($userName) {
-	$sql = "SELECT * from Student_Class where Sid = ?";
+	$sql = "SELECT Cname from Student_Class,Class where Sid = ? and Class.Cnumber = Student_Class.Cnumber";
 
 	$stmt = self::$connection->prepare($sql);
 
@@ -119,21 +119,180 @@ function didUserTakeClass($userName, $cNumber) {
 	}
 }
 
-//TODO select statement that verifies user can provide class evaluation
+//TODO select statement that verifies user can provide class evaluation; have they already answered
 // User can only provide one class evaluation
 function isUserEvaluationValid($userName, $evaluationAnswers) {
+	$sql = "SELECT Answer from Question where Sid = ? and Cnumber = ?";
+
+	$stmt = self::$connection->prepare($sql);
+
+	if( !$stmt->bind_param("ss", $userName, $cNumber) ) {
+		return false;
+	}
+
+	if( !$stmt->execute() ) {
+		return false;
+	}
+
+	if(!($res = $stmt -> get_result())) {
+		return false;
+	}
+
+	$res_size = sizeof($res->fetch_all());
+
+	$stmt->close();
+
+	if($res_size == 0){
+		return true;
+	} else {
+		return false;
+	}
 
 }
 
-//TODO select statement that loads the evaluation questions for each course
-function getEvaluationQuestion($course) {
+//if no one from the class has answered the question, you can assume it is not on the eval
+function isQuestionInCourseEval($fname,$lname, $cName) {
+	$sql = "SELECT distinct Answer from Class,Question where Class.Cnumber = Question.Cnumber and Fname = $fname and Lname = $lname and Cname = ?";
 
+	$stmt = self::$connection->prepare($sql);
+
+	if( !$stmt->bind_param("ss", $userName, $cNumber) ) {
+		return false;
+	}
+
+	if( !$stmt->execute() ) {
+		return false;
+	}
+
+	if(!($res = $stmt -> get_result())) {
+		return false;
+	}
+
+	$res_size = sizeof($res->fetch_all());
+
+	$stmt->close();
+
+	if($res_size == 1 and $res[0] == ''){
+		return false;
+	} else {
+		return true;
+	}
+
+}
+
+//TODO converts course name to number
+// returns a 1 by 1 array with an integer, but I want to return just the integer
+function courseNameToNumber($course) {
+	$sql = "SELECT distinct Cnumber from Class where Cname = ?";
+
+	$stmt = self::$connection->prepare($sql);
+
+	if( !$stmt->bind_param("s", $userName) ) {
+		return false;
+	}
+
+	if( !$stmt->execute() ) {
+		return false;
+	}
+
+	if(!($res = $stmt -> get_result())) {
+		return false;
+	}
+
+	$stmt->close();
+	return $res->fetch_all();
+}
+
+
+//TODO select statement that loads the evaluation questions for each course
+// returns a 1 by 1 array with the text of the question, but I wan't just the text of the question
+function getEvaluationQuestion($courseNumber) {
+	$sql = "SELECT Qcontent from Question_Evaluation,Question where Question_Evaluation.Qnumber = Question.Qnumber and Cnumber = ?";
+
+	$stmt = self::$connection->prepare($sql);
+
+	if( !$stmt->bind_param("s", $userName) ) {
+		return false;
+	}
+
+	if( !$stmt->execute() ) {
+		return false;
+	}
+
+	if(!($res = $stmt -> get_result())) {
+		return false;
+	}
+
+	$stmt->close();
+	return $res->fetch_all();
 }
 
 //TODO insert evaluation answer
-function insertEvaluationAnswer() {
+function insertEvaluationAnswer($studentId,$classId,$questionNum,$ans) {
+$sql = "INSERT INTO Question
+VALUES ($studentId, $classId,$questionNum,$ans) ";
+
+$stmt = self::$connection->prepare($sql);
+
+if ($stmt->execute()) {
+   	echo "Submitted!";
+} else {
+    echo "Error with Submission.";
+}
+}
+
+//Student View
+
+//Find type of question given Question number
+//Return 1 by 1 array with question type
+function questionType($qNumber) {
+	$sql = "SELECT Qtype from Question_Evaluation where Qnumber = ?";
+
+	$stmt = self::$connection->prepare($sql);
+
+	if( !$stmt->bind_param("s", $userName) ) {
+		return false;
+	}
+
+	if( !$stmt->execute() ) {
+		return false;
+	}
+
+	if(!($res = $stmt -> get_result())) {
+		return false;
+	}
+
+	$stmt->close();
+	return $res->fetch_all();
+}
+
+//finds numerator of Agree Disagree task in student view
+function agreeDisagreeNumerator($Qnumber) {
 
 }
+
+// finds median of unordered varchar array
+function medianVarChar($array){
+$length = count($array);
+if ($length == 1) {
+	return $array(0);
+}
+$iterator = 0;
+$newarray = array($length);
+while ($iterator <= $length) {
+	$newarray[$iterator] = (float) $array[$iterator];
+}
+sort($newarray);
+if ($length % 2 == 0){
+	return ($newarray[($length/2)-1] + $newarray[($length/2)+1])/2;
+}
+else {
+	return $newarray[$length-1];
+}
+
+}
+
+
 
 }
 ?> 
